202d9bd0cc012e260a3cf9541afb2bc42fffe407
ShaiMaytav <shaimaytav01@gmail.com>
2022-01-21 00:27:04 +0200

    made abstract interactable class

--- DesignPatterns_prjct/Assets/Scripts/Interactable.cs
+++ DesignPatterns_prjct/Assets/Scripts/Interactable.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactable : MonoBehaviour
+public abstract class Interactable : MonoBehaviour
 {
-    public 
+    public GameObject key;
+    public Player player;
+    private List<GameObject> playerInventory;
 
-    // Start is called before the first frame update
-    void Start()
+    public virtual void HandleInteraction()
     {
-        
-    }
+        playerInventory = player.inventory;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //Objects functionality
     }
+   
 }
